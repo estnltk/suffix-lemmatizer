a29e3b36eb84a7162362cb7964a4783732e56161
Aleksandr Tkachenko <alex.tk.fb@gmail.com>
2015-12-04 16:04:26 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     name = 'suffix-lemmatizer',
     description = 'Suffix Lemmatizer for Estonian',
     version = '1.0.0',
-    install_requires = ['docopt==0.6.2'],
     author = 'Alexander Tkachenko',
     packages = find_packages(),
     package_data = {'suffix_lemmatizer': ['data/*.bz2']},
