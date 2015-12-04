528b1e989de4f84519ac1c9bd13fbfdf9421d013
distorti <alex.tk.fb@gmail.com>
2015-12-04 11:42:01 +0200

    Fixed version 1.0 -> 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'suffix-lemmatizer',
     description = 'Suffix Lemmatizer for Estonian',
-    version = '1.0',
+    version = '1.0.0',
     install_requires = ['docopt==0.6.2'],
     author = 'Alexander Tkachenko',
     packages = find_packages(),
